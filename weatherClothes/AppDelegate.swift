495406771bd852732bedd5392fb7a7f07bfe80c7
Sue1215 <one.two.deculture12@icloud.com>
2019-04-28 14:07:27 +0900

    Initial Commit

---
+++ weatherClothes/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  weatherClothes
+//
+//  Created by sue on 2019/04/28.
+//  Copyright © 2019 sue. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+
+
+}
+
---
+++ weatherClothes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherClothes
+//
+//  Created by sue on 2019/04/28.
+//  Copyright © 2019 sue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
